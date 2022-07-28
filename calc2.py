85e1d800646698b2006078173bf257d0270bf62e
Kalu Udo <udokalu392@gmail.com>
2022-07-28 12:37:39 +0100

    added some great changes

---
+++ calc2.py
@@ -0,0 +1,5 @@
+def calcualator (n1,n2):
+	addition = n1 + n2
+	subtraction = n1 - n2
+	multiplication = n1 * n2
+	division = n1 / n2
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-def callback():
-  print("Hello there!")
-  print("printing second line")
+def message(name):
+    print(f"Hello {name},\nCongratulations! and welcome onbaord\nYou can resume anytime soon")
+message("Kalu Udo")
